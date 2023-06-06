a31f9e1cc6a8568aa56d992f61ac9a3bb9f145ab
Azatbek <124716757+Aza0602@users.noreply.github.com>
2023-06-07 00:12:40 +0600

    Delete course-four-lesson-three.sql
    delete

--- src/main/resources/liquibase/scripts/course-four-lesson-three.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
-
--- changeset aelchikushuulu:1
-SELECT * FROM students WHERE name='';
-
--- changeset aelchikushuulu:2
-SELECT * FROM faculties WHERE color = '' AND name = ''
